6b7aab3f1a59c45d6c056dcdd4961978ae53fb13
Andy Fleming <afleming@gmail.com>
2015-09-20 23:38:28 -0500

    Workaround for P5040 silicon errata
    retry stalled transactions, as they will normally succeed the second time.
    
    Conflicts:
    	drivers/usb/host/ehci-hcd.c

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -1179,12 +1179,17 @@ done:
 static int _ehci_submit_bulk_msg(struct usb_device *dev, unsigned long pipe,
 				 void *buffer, int length)
 {
+	int result;
+	int retries = 5;
 
 	if (usb_pipetype(pipe) != PIPE_BULK) {
 		debug("non-bulk pipe (type=%lu)", usb_pipetype(pipe));
 		return -1;
 	}
-	return ehci_submit_async(dev, pipe, buffer, length, NULL);
+	do {
+		result = ehci_submit_async(dev, pipe, buffer, length, NULL);
+	 } while (--retries > 0 && (dev->status & USB_ST_STALLED));
+	 return result;
 }
 
 static int _ehci_submit_control_msg(struct usb_device *dev, unsigned long pipe,
@@ -1192,6 +1197,8 @@ static int _ehci_submit_control_msg(struct usb_device *dev, unsigned long pipe,
 				    struct devrequest *setup)
 {
 	struct ehci_ctrl *ctrl = ehci_get_ctrl(dev);
+	int result;
+	int retries = 5;
 
 	if (usb_pipetype(pipe) != PIPE_CONTROL) {
 		debug("non-control pipe (type=%lu)", usb_pipetype(pipe));
@@ -1203,7 +1210,10 @@ static int _ehci_submit_control_msg(struct usb_device *dev, unsigned long pipe,
 			dev->speed = USB_SPEED_HIGH;
 		return ehci_submit_root(dev, pipe, buffer, length, setup);
 	}
-	return ehci_submit_async(dev, pipe, buffer, length, setup);
+	do {
+	 	result = ehci_submit_async(dev, pipe, buffer, length, setup);
+	 } while (--retries > 0 && (dev->status & USB_ST_STALLED));
+	 return result;
 }
 
 struct int_queue {
