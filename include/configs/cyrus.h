051ec45e308637ea358a025f847a44f454172c9f
Andy Fleming <afleming@gmail.com>
2015-10-02 18:00:28 -0500

    Disable Memory test

--- include/configs/cyrus.h
+++ include/configs/cyrus.h
@@ -134,7 +134,7 @@
 #endif
 
 /* test POST memory test */
-#define CONFIG_POST CONFIG_SYS_POST_MEMORY
+#undef CONFIG_POST
 #define CONFIG_SYS_MEMTEST_START	0x00200000	/* memtest works on */
 #define CONFIG_SYS_MEMTEST_END		0x00400000
 #define CONFIG_SYS_ALT_MEMTEST
