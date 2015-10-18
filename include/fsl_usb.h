173aca7aeb0aaaad1e470cf50fd06c74480f4a4b
Andy Fleming <afleming@gmail.com>
2015-10-18 03:32:01 -0500

    Remove not yet supported chip types

--- include/fsl_usb.h
+++ include/fsl_usb.h
@@ -104,12 +104,8 @@ static inline bool has_erratum_a006261(void)
 		return IS_SVR_REV(svr, 1, 0) || IS_SVR_REV(svr, 2, 0);
 	case SVR_T4240:
 	case SVR_T4160:
-	case SVR_T4080:
 		return IS_SVR_REV(svr, 1, 0) || IS_SVR_REV(svr, 2, 0);
 	case SVR_T1040:
-		return IS_SVR_REV(svr, 1, 0);
-	case SVR_T2080:
-	case SVR_T2081:
 		return IS_SVR_REV(svr, 1, 0);
 	case SVR_P5040:
 		return IS_SVR_REV(svr, 1, 0);
